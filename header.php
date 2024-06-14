a5c42523b08b2f4f810518fca2de6f59d4b0ba8b
Tomás Alves <tomasalves35@gmail.com>
2024-06-14 23:05:46 +0100

    Home Page

---
+++ header.php
@@ -0,0 +1,4 @@
+<head>
+    <title>Projeto - Tomás Alves</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
---
+++ navbar.php
@@ -0,0 +1,19 @@
+<div id="navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Projeto Computação em Nuvem</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarNav" class="collapse navbar-collapse">
+            <ul class="navbar-nav
+                ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="motas.php">Motas</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
